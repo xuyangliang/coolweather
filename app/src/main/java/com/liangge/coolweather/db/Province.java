789cc0a49477412b8ffed06e4a314ac1ef4b5683
XuyangLiang <2685359627@qq.com>
2018-05-04 13:03:44 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/liangge/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.liangge.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 梁哥 on 2018/5/4.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    private String provinceName;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private int provinceCode;
+
+}
